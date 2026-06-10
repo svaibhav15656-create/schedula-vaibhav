3de898ec78a8ac6eda8ad11d546c12e115688998
S Vaibhav <s.vaibhav15656@gmail.com>
2026-06-10 12:48:02 +0530

    feat: configure neon db for :wqproduction
    git push origin main --force#

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -10,28 +10,20 @@ import { UsersModule } from './users/users.module';
   imports: [AuthModule, UsersModule],
 import { DoctorModule } from './doctor/doctor.module';
 import { PatientModule } from './patient/patient.module';
-import { User } from './users/entities/user.entity';
-import { DoctorProfile } from './doctor/entities/doctor-profile.entity';
-import { PatientProfile } from './patient/entities/patient-profile.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get<string>('DB_HOST', 'localhost'),
-        port: config.get<number>('DB_PORT', 5432),
-        username: config.get<string>('DB_USERNAME', 'postgres'),
-        password: config.get<string>('DB_PASSWORD', 'postgres'),
-        database: config.get<string>('DB_NAME', 'schedula'),
-        entities: [User, DoctorProfile, PatientProfile],
-        migrations: [__dirname + '/database/migrations/*{.ts,.js}'],
-        synchronize: false,
-        migrationsRun: false,
-      }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'ep-sweet-shape-aprw84nl-pooler.c-7.us-east-1.aws.neon.tech',
+      port: 5432,
+      username: 'neondb_owner',
+      password: 'npg_VMxmhk7F0lpO',
+      database: 'neondb',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
+      ssl: { rejectUnauthorized: false },
     }),
     AuthModule,
     UsersModule,
